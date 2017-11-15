9818bf7bdf3c43a24be9a237b56b251ad53dca0c
Nich Bailey <nichbailey@gmail.com>
2017-11-15 19:17:30 +0000

    added get_me method

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,4 +1,5 @@
 require 'httparty'
+require 'json'
 
 class Kele
     include HTTParty
@@ -8,5 +9,10 @@ class Kele
         response = self.class.post("https://www.bloc.io/api/v1/sessions", @options)
         raise "invalid email or password" if response.code == 401
         @auth_token = response["auth_token"]
+    end
+    
+    def get_me
+        response = self.class.get("https://www.bloc.io/api/v1/users/me", headers: { "authorization" => @auth_token })
+        JSON.parse(response.body)
     end
 end
