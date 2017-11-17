5ff88b743eb83a7a5892c29a8447343dfad42f28
Nich Bailey <nichbailey@gmail.com>
2017-11-17 04:36:08 +0000

    viewing and creating messages

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,6 +1,8 @@
 require 'httparty'
 require 'json'
 require './lib/roadmap.rb'
+require 'rest_client'
+require 'rubygems'
 
 class Kele
     include HTTParty
@@ -21,6 +23,22 @@ class Kele
     def get_mentor_availability(mentor_id)
         response = self.class.get("https://www.bloc.io/api/v1/mentors/#{mentor_id}/student_availability", headers: { "authorization" => @auth_token })
         JSON.parse(response.body)
+    end
+    
+    def get_messages(*page)
+        page_string = page.to_s
+        response = self.class.get("https://www.bloc.io/api/v1/message_threads", headers: { "authorization" => @auth_token }, values: {"page": page_string})
+        JSON.parse(response.body)
+    end
+    
+    def create_message(email, recipient_id, subject, stripped_text)
+        response = self.class.post("https://www.bloc.io/api/v1/messages", headers: { "authorization" => @auth_token }, body: {
+            "sender": email, 
+            "recipient_id": recipient_id,
+            "subject": subject,
+            "stripped-text": stripped_text
+        })
+        response.success? puts "Message Sent"
     end
     
 end
